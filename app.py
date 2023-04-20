b675d8f2753ce0a46e1d138d09673dca8027ea86
葉晏愷 <kevin.yeh93@gmail.com>
2023-04-20 16:14:41 +0800

    2023/04/20

--- app.py
+++ app.py
@@ -13,18 +13,30 @@ def name(name):
 
 @app.route("/number/<int:number>")
 def number(number):
+    x=[i for i in range(number)]
     print('Type:',type(number))
-    return f"{number}"
+    return f"{x}"
 
 @app.route("/page")
-def email():
-    email = request.args.get("email")
-    password = request.args.get("password")
-    return f"{email},{password}"
+def page():
+    x="1234"
+    dict1={"Student ID":41141216,"Name":"kevin"}
+    return render_template("page.html",a=x,b=dict1)
 
-@app.route("/index")
-def index():
-    return render_template('index.html')
+@app.route("/login",methods=["GET","POST"])
+def login():
+    if request.method == 'POST':
+        name=request.form.get('account')
+        password=request.form.get('password')
+        if name =="admin" and password =="1234":
+            type="成功"
+            return render_template("page2.html",id=name,ps=password,type=type)
+        else:
+            type="失敗"
+            return render_template("login.html",type=type)
+    else:      
+        return render_template('login.html')
 
+     
 if __name__== '__main__':
     app.run(debug=True)
